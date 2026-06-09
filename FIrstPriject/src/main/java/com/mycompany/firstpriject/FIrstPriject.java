1229b70601ac594caaea5b023ce4d8b69b20a09f
YashKachaRK <ykacha243@rku.ac.in>
2026-06-10 00:36:41 +0530

    Interface learn

--- FIrstPriject/src/main/java/com/mycompany/firstpriject/FIrstPriject.java
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/FIrstPriject.java
@@ -3,36 +3,36 @@
  */
 package com.mycompany.firstpriject;
 
-abstract class A {
-
-    A() {
-        System.out.println("Parent coustructor");
-    }
-
-    abstract void show(); // abstact class ni body no hoi without body j hoi
-
-    abstract void display();
+interface A{
+    int num = 2;
+    void show(); // by defult public j hoi 
+    // interace no object no banavi sake;
+    
+    default void run(){
+        System.out.println("Run");
+    }// using defult apade ahiya j body api saki chhi
+    
 }
-
-class B extends A {
-    B(){
-        super();
-        System.out.println("Child class");
+interface  C {
+    void display();
+}
+// interface mate implements no use thay chhe
+class B implements A , C{
+    public void show (){
+        System.out.println("Show");
     }
-    public void show() {
-        System.out.println("Hey!! ");
-    }
-
-    public void display() {
-        System.out.println("DIsplay ");
+    public void display(){
+        System.out.println("Class B");
     }
 }
 
 public class FIrstPriject {
 
     public static void main(String[] args) {
-        B obj = new B();
-        obj.show();
+       B b = new B();
+       b.show();
+       b.display();
+       b.run(); 
     }
 
 }
