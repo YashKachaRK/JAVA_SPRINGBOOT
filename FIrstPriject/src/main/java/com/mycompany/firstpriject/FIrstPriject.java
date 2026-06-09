129b3d0fc3c88d1e7cbfbf2b686ffbc16480cf66
YashKachaRK <ykacha243@rku.ac.in>
2026-06-09 18:07:45 +0530

    Access modifiers

--- FIrstPriject/src/main/java/com/mycompany/firstpriject/FIrstPriject.java
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/FIrstPriject.java
@@ -5,66 +5,28 @@ package com.mycompany.firstpriject;
 
 import java.util.Scanner;
 
-/**
- *
- * @author yashk
- */
-//class Person {
-////    String grade; 
-////    Person(String n) {
-////        System.out.println("Constructor called ");
-////        System.out.println(n);
-////
-////    }
-////
-////    Person(int a) {
-////        System.out.println(a);
-////    }
-////    Person (String  grade){
-////        this.grade = grade ;
-////    }
-////    void display (){
-////        System.out.println(grade);
-////    }
-//    Person(){
-//        this("Yash");
-//        
-//    }
-//    Person(String name){
-//        System.out.println(name);
-//    }
-//}
+class Person {
 
-class Student {
+    private int age;
 
-    String name;
-    int age;
-
-    Student(String n, int a) {
-
-        name = n;
-        age = a;
+    void setAge(int age) {
+        this.age = age;
     }
 
-    Student(Student s) {
-
-        name = s.name;
-        age = s.age;
+    int getAge() {
+        return age;
     }
+
 }
 
 public class FIrstPriject {
 
     public static void main(String[] args) {
-        Student s1
-                = new Student("Yash", 21);
 
-        Student s2
-                = new Student(s1);
+        Person p = new Person();
+        p.setAge(25);
+        System.out.println(p.getAge());
 
-        System.out.println(s2.name);
-
-//        Person  p1 = new Person(25);
     }
 
 }
