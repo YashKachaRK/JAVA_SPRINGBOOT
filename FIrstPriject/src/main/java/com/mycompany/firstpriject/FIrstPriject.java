414d4147e395c7226167aa69952616394f7c7fdd
YashKachaRK <ykacha243@rku.ac.in>
2026-06-09 11:46:14 +0530

    First Project

---
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/FIrstPriject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstpriject;
+
+/**
+ *
+ * @author yashk
+ */
+public class FIrstPriject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
