d9297a5b157c747c089cbe3d298639e581fb47f5
YashKachaRK <ykacha243@rku.ac.in>
2026-06-11 17:09:08 +0530

    Lern LinkedList_learn

--- FIrstPriject/src/main/java/com/mycompany/firstpriject/Multithreading.java
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/Multithreading.java
@@ -1,4 +1,12 @@
 package com.mycompany.firstpriject;
 
-public class Multithreading {
+public class Multithreading extends  Thread{
+    public  void  run(){
+        System.out.println("Running...........");
+    }
+
+    static void main(String[] args) {
+        Multithreading  m1  = new Multithreading();
+        m1.start();
+    }
 }
