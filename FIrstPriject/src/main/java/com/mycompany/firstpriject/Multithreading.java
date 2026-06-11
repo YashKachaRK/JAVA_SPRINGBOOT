1fa8068bf860aae5a9f829f1ae9383d739ca5d0f
YashKachaRK <ykacha243@rku.ac.in>
2026-06-11 17:21:32 +0530

    Lern Thread

--- FIrstPriject/src/main/java/com/mycompany/firstpriject/Multithreading.java
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/Multithreading.java
@@ -1,12 +1,28 @@
 package com.mycompany.firstpriject;
 
-public class Multithreading extends  Thread{
-    public  void  run(){
-        System.out.println("Running...........");
+class DownloadTask extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Downloading..........." + i);
+        }
+    }
+}
+
+public class Multithreading extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Running..........." + i);
+        }
     }
 
     static void main(String[] args) {
-        Multithreading  m1  = new Multithreading();
+        Multithreading m1 = new Multithreading();
         m1.start();
+
+        DownloadTask d1 = new DownloadTask();
+        d1.start();
     }
+
 }
