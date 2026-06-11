0e13bf1fb46f1f720b9e0006efd050a4e09a6148
YashKachaRK <ykacha243@rku.ac.in>
2026-06-11 17:51:01 +0530

    Lern Thread

--- FIrstPriject/src/main/java/com/mycompany/firstpriject/Thread_Cunstroctor.java
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/Thread_Cunstroctor.java
@@ -1,4 +1,23 @@
 package com.mycompany.firstpriject;
 
+
+class Mythr extends Thread {
+    public Mythr(String name) {
+        super(name);
+    }
+
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Run.........");
+        }
+    }
+}
+
 public class Thread_Cunstroctor {
+    public static void main(String[] args) {
+        Mythr t = new Mythr("Yash");
+        t.start();
+        System.out.println(t.getName());
+        System.out.println("Thread is id :- "+ t.getId());
+    }
 }
