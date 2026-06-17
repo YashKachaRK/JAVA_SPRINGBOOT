7929ac406c5e18eec7345b01e1f565a1005e12b1
YashKachaRK <ykacha243@rku.ac.in>
2026-06-17 11:21:56 +0530

    Anonymous class

---
+++ FIrstPriject/src/main/java/com/mycompany/firstpriject/LambdaExpressions.java
@@ -0,0 +1,27 @@
+package com.mycompany.firstpriject;
+
+
+import java.lang.classfile.Interfaces;
+
+interface Demo {
+    void math1();
+}
+
+public class LambdaExpressions {
+    static void main(String[] args) {
+        // Anonymous class'
+//        Demo obj = new Demo(){
+//
+//            @Override
+//            public void math1() {
+//                System.out.println("Math1");
+//            }
+//        };
+//        obj.math1();
+
+
+
+        
+
+    }
+}
